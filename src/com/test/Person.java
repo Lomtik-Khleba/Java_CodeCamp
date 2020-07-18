a3fe577fd33d0513ce97c9325cd22f3269db6228
Chop <z.ilya.007@gmail.com>
2020-07-18 20:45:27 +0300

    Inital commit

--- src/com/test/Person.java
+++ src/com/test/Person.java
@@ -3,12 +3,15 @@ package com.test;
 public class Person {
 
     public static Name personName;
+    private static int personCounter;
 
     public Person(Name personName){
         Person.personName = personName;
     }
 
-    public Person(){}
+    public Person(){
+        personCounter++;
+    }
 
     public String helloWorld() {
         return "Hello World";
@@ -16,5 +19,9 @@ public class Person {
 
     public String hello(String name) {
         return "Hello " + name;
+    }
+
+    public static int numberOfPersons() {
+        return personCounter;
     }
 }
--- src/com/test/PersonTest.java
+++ src/com/test/PersonTest.java
@@ -1,6 +1,9 @@
 package com.test;
 
+import org.apache.commons.logging.Log;
 import org.junit.Test;
+
+import java.util.function.Predicate;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,5 +20,30 @@ public class PersonTest {
         Person person = new Person();
         assertEquals("Hello Ilya", person.hello("Ilya"));
     }
+
+    @Test
+    public void shouldReturnNumberOfPersons(){
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+
+        assertEquals(3, Person.numberOfPersons());
+    }
+
+    @Test
+    public void demonstrateArrays(){
+        for(LoggingLevel state: LoggingLevel.values()){
+            if(state == LoggingLevel.PENDING){
+
+            }
+            if(state == LoggingLevel.PROCESSED){
+
+            }
+            if(state == LoggingLevel.PROCESSING){
+
+            }
+        }
+    }
+
 
 }
